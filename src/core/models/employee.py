f83e45c7b54f1c92503f229fdf94f49bdc6773b1
igorkonov <igorkonov1993@gmail.com>
2024-05-31 16:52:59 +0300

    create model employee

---
+++ src/core/models/employee.py
@@ -0,0 +1,22 @@
+from sqlalchemy import String, LargeBinary
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from .base import Base
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    fullname: Mapped[str] = mapped_column(String(50), unique=True)
+    age: Mapped[int] = mapped_column()
+    email: Mapped[str | None] = mapped_column(String(50), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(1024), unique=True)
+    refresh_token: Mapped[str | None] = mapped_column()
+    is_active: Mapped[bool] = mapped_column(default=True)
+
+    def __str__(self):
+        return self.fullname
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id}, fullname={self.fullname!r}, age={self.age!r})"
