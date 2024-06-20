ab1ba956f3bb29c9acb2a73c5400264cad97ec37
igorkonov <igorkonov1993@gmail.com>
2024-06-20 22:53:20 +0300

    update model employee

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -2,8 +2,12 @@ __all__ = (
     "db_helper",
     "Base",
     "Employee",
+    "Task",
+    "EmployeeRelationMixin",
 )
 
 from .db_helper import db_helper
 from .base import Base
 from .employee import Employee
+from .task import Task
+from .mixin import EmployeeRelationMixin
--- src/core/models/employee.py
+++ src/core/models/employee.py
@@ -1,19 +1,25 @@
-from sqlalchemy import String, LargeBinary
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING, List
+
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.orm import mapped_column
 
 from .base import Base
 
+if TYPE_CHECKING:
+    from .task import Task
+
 
 class Employee(Base):
-    __tablename__ = "employees"
 
     fullname: Mapped[str] = mapped_column(String(50), unique=True)
-    age: Mapped[int] = mapped_column(String(10))
+    age: Mapped[int]
     email: Mapped[str | None] = mapped_column(String(50), unique=True)
     hashed_password: Mapped[str] = mapped_column(String(1024), unique=True)
-    refresh_token: Mapped[str | None] = mapped_column()
-    is_active: Mapped[bool] = mapped_column(default=True)
+    refresh_token: Mapped[str | None]
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    tasks: Mapped[List["Task"]] = relationship(back_populates="employee")
 
     def __str__(self):
         return self.fullname
