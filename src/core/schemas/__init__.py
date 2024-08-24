ab5ccbc55d93614b300910642eb5211d0e9f7a9d
igorkonov <igorkonov1993@gmail.com>
2024-08-24 11:30:33 +0300

    update schemas employee

---
+++ src/core/schemas/__init__.py
@@ -0,0 +1,15 @@
+__all__ = (
+    "EmployeeRequest",
+    "EmployeeResponse",
+    "TaskRequest",
+    "TaskResponse",
+)
+
+from .employee import (
+    EmployeeRequest,
+    EmployeeResponse,
+)
+from .task import (
+    TaskRequest,
+    TaskResponse,
+)
--- src/core/schemas/employee.py
+++ src/core/schemas/employee.py
@@ -1,22 +1,44 @@
-from pydantic import BaseModel
-from pydantic import ConfigDict
+from typing import Optional, List
+from pydantic import BaseModel, ConfigDict, field_validator
+from pydantic.networks import EmailStr
+
+from .task import TaskResponse
 
 
-class EmployeeBase(BaseModel):
-    fullname: str
-    age: int
-    email: str | None
-    hashed_password: str
-    refresh_token: str | None
-    is_active: bool
+class EmployeeRequest(BaseModel):
+    """
+    Представляет структуру запроса для создания или обновления сотрудника.
+    """
+
+    fullname: str = None
+    position: str = None
+    age: Optional[int] = None
+    email: Optional[EmailStr] = None
+    hashed_password: Optional[str] = None
+    refresh_token: Optional[str] = None
+    is_active: bool = True
+
+    @field_validator("email", mode="before")
+    def validate_email(cls, v):
+        return v or None  # Заменяем пустую строку на None
+
+    @field_validator("age", mode="before")
+    def validate_age(cls, v):
+        if v is not None and (v < 18 or v > 150):
+            raise ValueError(
+                "Возраст сотрудника должен быть в пределах от 18 до 150 лет"
+            )
+        return v
 
 
-class EmployeeCreate(EmployeeBase):
-    pass
+class EmployeeResponse(EmployeeRequest):
+    """
+    Представляет структуру схемы, используемую для чтения данных о сотруднике.
+    """
 
+    model_config = ConfigDict(from_attributes=True)
 
-class EmployeeRead(EmployeeBase):
-    model_config = ConfigDict(
-        from_attributes=True,
-    )
     id: int
+    tasks: Optional[List[TaskResponse]] = (
+        None  # Список идентификаторов задач сотрудника
+    )
